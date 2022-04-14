bd78256da17df6bb5b5e6331e30630d7df51e8ca
Roman <77742141+rumpear@users.noreply.github.com>
2022-04-14 22:20:17 +0300

    Update btnUp.js

--- src/js/btnUp.js
+++ src/js/btnUp.js
@@ -1,18 +1,40 @@
 import throttle from 'lodash.throttle';
 
-const btnUpRef = document.querySelector('.btn-up');
+const btnUpElem = document.createElement('button');
+btnUpElem.setAttribute('type', 'button');
+btnUpElem.classList.add('btn-up');
+btnUpElem.classList.add('is-hidden');
 
-btnUpRef.addEventListener('click', () => {
+const svgElem = document.createElementNS('http://www.w3.org/2000/svg', 'svg');
+const useElem = document.createElementNS('http://www.w3.org/2000/svg', 'use');
+useElem.setAttributeNS(
+  'http://www.w3.org/1999/xlink',
+  'href',
+  '/sprite.5ec50489.svg#icon-up-arrow',
+);
+
+svgElem.setAttribute('width', '20');
+svgElem.setAttribute('height', '20');
+svgElem.classList.add('team-social__icon');
+
+svgElem.appendChild(useElem);
+btnUpElem.appendChild(svgElem);
+document.body.append(btnUpElem);
+
+const upToTop = () => {
   document.documentElement.scrollTop = 0;
   document.body.scrollTop = 0;
-});
+};
 
-function scrollFunction() {
+btnUpElem.addEventListener('click', upToTop);
+
+const scrollFunction = () => {
   if (document.body.scrollTop > 200 || document.documentElement.scrollTop > 200) {
-    btnUpRef.classList.remove('is-hidden');
+    btnUpElem.classList.remove('is-hidden');
   } else {
-    btnUpRef.classList.add('is-hidden');
+    btnUpElem.classList.add('is-hidden');
   }
-}
+};
 
-window.addEventListener('scroll', throttle(scrollFunction, 600));
+window.addEventListener('scroll', throttle(scrollFunction, 500));
+// window.addEventListener('scroll', scrollFunction, { passive: true });
